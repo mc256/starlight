fada7566f63c43a75dad03633e0f520073874772
Jun Lin Chen <webmaster@mc256.dev>
2022-10-11 17:21:16 +0000

    FIX: downgrade urfave/cli/v2
    Newer version introduce unexpected string slice behavior which lead to
    issue #24
    Down grading urfave/cli/v2 to v2.3.0
    
    Planning to add automatic testing pipeline for this project

--- demo/reset.sh
+++ demo/reset.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
+## This script reset the experiment environment
+## Please cd into the project's root directory and run this script using this command:
+##  sudo ./demo/reset.sh
 
 STARLIGHT_SNAPSHOTTER_ROOT=/var/lib/starlight-grpc/
 
+# Stop Starlight and containerd
 systemctl stop containerd
 systemctl stop starlight
-sudo pkill -9 'containerd' | true
-sudo pkill -9 'starlight-grpc' | true
+pkill -9 'containerd' | true
+pkill -9 'starlight-grpc' | true
 
+
+# Clear containerd folder
 rm -rf /var/lib/containerd
 
+# Clear starlight folder
 if [ -d "${STARLIGHT_SNAPSHOTTER_ROOT}sfs/" ] ; then
     find "${STARLIGHT_SNAPSHOTTER_ROOT}sfs/" \
          -maxdepth 1 -mindepth 1 -type d -exec sudo umount -f "{}/m" \;
 fi
 rm -rf "${STARLIGHT_SNAPSHOTTER_ROOT}"*
 
+# Remove Redis data folder
 rm -rf /tmp/test-redis-data
 
-systemctl start starlight
-systemctl start containerd
+
+# Restart the service
+./out/starlight-grpc  run --server=starlight.yuri.moe --log-level=debug &
+containerd &
