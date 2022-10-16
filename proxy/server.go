2002acb4ede01a1bea0676937ca06221957dfa53
Jun Lin Chen <webmaster@mc256.com>
2022-10-15 22:45:49 -0400

    fix: optional from

--- proxy/server.go
+++ proxy/server.go
@@ -124,7 +124,7 @@ func (a *Server) starlight(w http.ResponseWriter, req *http.Request) {
 	switch action {
 	case "delta-image":
 		f, t := q.Get("from"), q.Get("to")
-		if f == "" || t == "" {
+		if t == "" {
 			a.error(w, req, "missing parameters")
 			return
 		}
