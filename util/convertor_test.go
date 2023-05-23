7f14aeca6d5fcaf74e38ae37df4666d05b83dbf7
Jun Lin Chen <webmaster@mc256.dev>
2023-05-23 02:11:55 +0000

    add environment variables

--- util/convertor_test.go
+++ util/convertor_test.go
@@ -3,11 +3,13 @@ package util
 import (
 	"context"
 	"fmt"
+	"os"
 	"testing"
 
 	"github.com/google/go-containerregistry/pkg/authn"
 	"github.com/google/go-containerregistry/pkg/name"
 	"github.com/google/go-containerregistry/pkg/v1/remote"
+	"github.com/mc256/starlight/test"
 )
 
 const (
@@ -30,12 +32,19 @@ func TestConvertorConstructor(t *testing.T) {
 		t.Fatal(err)
 	}
 	fmt.Println(c)
-	return
+
 }
 
 // TestToStarlightImage Image does requires network connection
-func TestToStarlightImage(t *testing.T) {
-	t.Skip("for dev only")
+func TestToStarlightImageGoharbor(t *testing.T) {
+	if test.HasLoginStarlightGoharbor() == false {
+		t.Skip(">>>>> Skip: no container registry credentials for goharbor")
+	}
+
+	const (
+		srcRef = "docker.io/library/redis:6.2.1"
+		dstRef = "harbor.yuri.moe/public/redis:6.2.1"
+	)
 
 	ctx := context.Background()
 
@@ -52,6 +61,37 @@ func TestToStarlightImage(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
+}
 
-	return
+func TestToStarlightImageECR(t *testing.T) {
+	if test.HasLoginAWSECR() == false {
+		t.Skip(">>>>> Skip: no container registry credentials for ECR")
+	}
+
+	if os.Getenv("TEST_ECR_IMAGE_FROM") == "" {
+		t.Skip(">>>>> Skip: no TEST_ECR_IMAGE_FROM environment variable")
+	}
+
+	if os.Getenv("TEST_ECR_IMAGE_TO") == "" {
+		t.Skip(">>>>> Skip: no TEST_ECR_IMAGE_TO environment variable")
+	}
+
+	srcRef := os.Getenv("TEST_ECR_IMAGE_FROM")
+	dstRef := os.Getenv("TEST_ECR_IMAGE_TO")
+
+	ctx := context.Background()
+
+	c, err := NewConvertor(ctx, srcRef, dstRef, []name.Option{}, []name.Option{}, []remote.Option{
+		remote.WithAuthFromKeychain(authn.DefaultKeychain),
+	}, "all")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = c.ToStarlightImage()
+	if err != nil {
+		t.Fatal(err)
+	}
 }
