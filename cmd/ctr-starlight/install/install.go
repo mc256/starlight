06257d338b9552e92716fbbf89b6255c6446ca06
Jun Lin Chen <webmaster@mc256.dev>
2023-07-23 19:17:58 +0000

    ADD: k3s install feature

--- cmd/ctr-starlight/install/install.go
+++ cmd/ctr-starlight/install/install.go
@@ -15,16 +15,47 @@ import (
 
 	"github.com/containerd/containerd/log"
 	srvconfig "github.com/containerd/containerd/services/server/config"
+	"github.com/mc256/starlight/client"
 	"github.com/mc256/starlight/util"
 	toml "github.com/pelletier/go-toml"
 
 	"github.com/urfave/cli/v2"
 )
 
-func InstallWithContainerd(ctx context.Context, configPath string) error {
+func appendStarlightConfiguartion(ctx context.Context, socketAddress string, reader []byte) (output []byte, err error) {
+	var config srvconfig.Config
+	err = toml.Unmarshal(reader, &config)
+	if err != nil {
+		return nil, err
+	}
+
+	// add starlight config
+	if config.ProxyPlugins == nil {
+		config.ProxyPlugins = make(map[string]srvconfig.ProxyPlugin)
+	}
+
+	config.ProxyPlugins["starlight"] = srvconfig.ProxyPlugin{
+		Type:    "snapshot",
+		Address: socketAddress,
+	}
+
+	plugin := config.Plugins["io.containerd.grpc.v1.cri"]
+	cni := plugin.Get("cni").(*toml.Tree)
+	cni.Set("snapshotter", "starlight")
+
+	// marshal config file
+	output, err = toml.Marshal(config)
+	if err != nil {
+		return nil, err
+	}
+	return output, nil
+}
+
+func InstallWithContainerd(ctx context.Context, configPath, socketAddr string) error {
 	log.G(ctx).
 		WithField("configPath", configPath).
-		Info("install Starlight to containerd")
+		Info("installing Starlight to containerd")
+
 	// check config file exist
 	if _, err := os.Stat(configPath); err != nil {
 		if os.IsNotExist(err) {
@@ -35,94 +66,87 @@ func InstallWithContainerd(ctx context.Context, configPath string) error {
 			// generate default config file
 			defaultBuffer, err := exec.Command("containerd", "config", "default").Output()
 			if err != nil {
-				log.G(ctx).
-					WithError(err).
-					WithField("configPath", configPath).
-					Error("generate default config file failed")
-				return err
+				return fmt.Errorf("generate default config file failed: %w", err)
 			}
 
 			// create directory for config file
 			if err := os.MkdirAll(path.Dir(configPath), 0755); err != nil {
-				log.G(ctx).
-					WithError(err).
-					WithField("configPath", configPath).
-					Error("create directory for config file failed")
-				return err
+				return fmt.Errorf("create directory for config file failed: %w", err)
 			}
 
 			// write default config file
 			if err := ioutil.WriteFile(configPath, defaultBuffer, 0644); err != nil {
-				log.G(ctx).
-					WithError(err).
-					WithField("configPath", configPath).
-					Error("write default config file failed")
-				return err
+				return fmt.Errorf("write default config file failed: %w", err)
 			}
 		} else {
-			log.G(ctx).
-				WithField("configPath", configPath).
-				Error("check config file exist failed, do we need root permission?")
+			return fmt.Errorf("check config file exist failed (do we need root permission?): %w", err)
 		}
 	}
 
 	// add starlight config to containerd config
 	reader, err := os.ReadFile(configPath)
 	if err != nil {
-		log.G(ctx).
-			WithError(err).
-			WithField("configPath", configPath).
-			Error("open config file failed")
-		return err
+		return fmt.Errorf("open config file failed: %w", err)
 	}
 
-	var config srvconfig.Config
-	err = toml.Unmarshal(reader, &config)
+	// append starlight config
+	bytes, err := appendStarlightConfiguartion(ctx, socketAddr, reader)
 	if err != nil {
-		log.G(ctx).
-			WithError(err).
-			WithField("configPath", configPath).
-			Error("unmarshal config file failed")
-		return err
-	}
-
-	// add starlight config
-	config.ProxyPlugins["starlight"] = srvconfig.ProxyPlugin{
-		Type:    "snapshot",
-		Address: "/run/starlight/starlight-snapshotter.sock",
-	}
-
-	plugin := config.Plugins["io.containerd.grpc.v1.cri"]
-	cni := plugin.Get("cni").(*toml.Tree)
-	cni.Set("snapshotter", "starlight")
-
-	// marshal config file
-	bytes, err := toml.Marshal(config)
-	if err != nil {
-		log.G(ctx).
-			WithError(err).
-			Error("marshal config file failed")
-		return err
+		return fmt.Errorf("append starlight config to containerd config failed: %w", err)
 	}
 
 	// write config file
 	if err := ioutil.WriteFile(configPath, bytes, 0644); err != nil {
-		log.G(ctx).
-			WithError(err).
-			WithField("configPath", configPath).
-			Error("write config file failed")
-		return err
+		return fmt.Errorf("write config file failed: %w", err)
 	}
 
 	log.G(ctx).
-		WithField("configPath", configPath).
-		Info("install Starlight to containerd success")
+		Info("successfully install Starlight to containerd")
 
 	return nil
 }
 
-func InstallWithK3s(ctx context.Context, configPath string) error {
-	// check config file exist
+func InstallWithK3s(ctx context.Context, configPath, configTemplatePath, socketAddr, config string) error {
+	log.G(ctx).
+		WithField("configPath", configPath).
+		WithField("configTemplatePath", configTemplatePath).
+		Info("installing Starlight to containerd")
+
+	// check config files exist
+	if _, err := os.Stat(configPath); err != nil {
+		if os.IsNotExist(err) {
+			return fmt.Errorf("cannot find k3s config file, do we need root permission? or have you install k3s?: %w", err)
+		} else {
+			return fmt.Errorf("check config file exist failed, do we need root permission?: %w", err)
+		}
+	}
+
+	// read config file
+	reader, err := os.ReadFile(configPath)
+	if err != nil {
+		return fmt.Errorf("open config file failed: %w", err)
+	}
+
+	// append starlight config
+	bytes, err := appendStarlightConfiguartion(ctx, socketAddr, reader)
+	if err != nil {
+		return fmt.Errorf("append starlight config to k3s config failed: %w", err)
+	}
+
+	// write config template file
+	if err := ioutil.WriteFile(configTemplatePath, bytes, 0644); err != nil {
+		return fmt.Errorf("write config template file failed: %w", err)
+	}
+
+	// update starlight config
+	cfg, _, _, _ := client.LoadConfig(config)
+	cfg.Containerd = "/run/k3s/containerd/containerd.sock"
+	if err = cfg.SaveConfig(); err != nil {
+		return fmt.Errorf("update starlight config failed: %w", err)
+	}
+
+	log.G(ctx).
+		Info("successfully install Starlight to k3s")
 
 	return nil
 }
@@ -139,10 +163,14 @@ func Action(ctx context.Context, c *cli.Context) error {
 		return fmt.Errorf("you must specify excatly one runtime either --k3s or --containerd")
 	}
 	if setContainerd {
-		return InstallWithContainerd(ctx, c.String("containerd-config"))
+		// set containerd
+		return InstallWithContainerd(ctx, c.String("containerd-config"), c.String("starlight-socket"))
 	} else {
 		// set k3s
-		return InstallWithK3s(ctx, c.String("k3s-config"))
+		return InstallWithK3s(ctx,
+			c.String("k3s-config"), c.String("k3s-config-template"),
+			c.String("starlight-socket"), c.String("starlight-config"),
+		)
 	}
 
 }
@@ -169,14 +197,29 @@ func Command() *cli.Command {
 			&cli.BoolFlag{
 				Name:  "k3s",
 				Value: false,
-				Usage: "enable Starlight Daemon for k3s",
+				Usage: "enable Starlight Daemon for k3s and annotate the node with \"starlight./enable=true\"",
 			},
 			&cli.StringFlag{
 				Name:  "k3s-config",
-				Value: "/etc/rancher/k3s/config.yaml",
-				Usage: "k3s config file path. If the configuration does not exist, it will be created automatically using the default configuration",
+				Value: "/var/lib/rancher/k3s/agent/etc/containerd/config.toml",
+				Usage: "k3s config file path. This file is generated by k3s and cannot be modified.",
+			},
+			&cli.StringFlag{
+				Name:  "k3s-config-template",
+				Value: "/var/lib/rancher/k3s/agent/etc/containerd/config.toml.tmpl",
+				Usage: "k3s config template file path. If the template does not exist, it will be created automatically using the existing configration",
+			},
+			&cli.StringFlag{
+				Name:  "starlight-socket",
+				Value: "/run/starlight/starlight-snapshotter.sock",
+				Usage: "starlight snapshotter socket path for configuration",
+			},
+			&cli.StringFlag{
+				Name:  "starlight-config",
+				Value: "/etc/starlight/starlight-daemon.json",
+				Usage: "starlight config file path. If the configuration does not exist, it will be created automatically using the default configuration",
 			},
 		},
-		ArgsUsage: "[flags] [BaseImage] PullImage",
+		ArgsUsage: "(--containerd|--k3s)",
 	}
 }
